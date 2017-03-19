207aa14fa4ffbfceff8b46fac0d8439104714375
skipmodea1 <skipmodea1@outlook.com>
2017-03-19 17:51:40 +0100

    v1.0.5 (2017-03-13):
    - changed addon debugging info to kodi debugging info
    - using po-files
    - fixed url in addon.xml as per request
    - changed 'xbmc' to 'kodi' in some text
    - not using addon-name in xbmcaddon.Addon() anymore (thanks enen92)

--- resources/lib/powerunlimited_const.py
+++ resources/lib/powerunlimited_const.py
@@ -8,8 +8,8 @@ import xbmcaddon
 # Constants
 # 
 ADDON = "plugin.video.powerunlimited"
-SETTINGS = xbmcaddon.Addon(id=ADDON)
+SETTINGS = xbmcaddon.Addon()
 LANGUAGE = SETTINGS.getLocalizedString
-IMAGES_PATH = os.path.join(xbmcaddon.Addon(id=ADDON).getAddonInfo('path'), 'resources', 'images')
-DATE = "2016-06-05"
-VERSION = "1.0.5-SNAPSHOT"
+IMAGES_PATH = os.path.join(xbmcaddon.Addon().getAddonInfo('path'), 'resources', 'images')
+DATE = "2017-03-13"
+VERSION = "1.0.5"
