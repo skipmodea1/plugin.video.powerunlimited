b16474cd8b9fab21bc5157cb35d1776fa507b7fb
skipmodea1 <skipmodea1@outlook.com>
2018-01-20 19:41:35 +0100

    v1.0.7 (2018-01-20)
    - removed looking for video dialogue
    - addon now works in kode python 2 and should also work in python 3 (!!)
    once all dependencies work in python 3.
    Kudo's to the python future package for making this possible. Kudo's to
    RomanVM for the help.
    - improved the finding of thumbnails

--- resources/lib/powerunlimited_const.py
+++ resources/lib/powerunlimited_const.py
@@ -15,8 +15,8 @@ SETTINGS = xbmcaddon.Addon()
 LANGUAGE = SETTINGS.getLocalizedString
 IMAGES_PATH = os.path.join(xbmcaddon.Addon().getAddonInfo('path'), 'resources', 'images')
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-DATE = "2018-01-03"
-VERSION = "1.0.7-SNAPSHOT"
+DATE = "2018-01-20"
+VERSION = "1.0.7"
 
 
 if sys.version_info[0] > 2:
